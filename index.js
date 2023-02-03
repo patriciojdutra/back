7933db86998f539204e8eb56d0dde256ff3fe101
Patricio Dutra <patriciojdutra@gmail.com>
2023-02-02 22:19:37 -0300

    mapeamento endpoints

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ server.use(express.json())
 const db = require('./src/data/db')
 
 server.get('/user/:id', async (req, res) => {
-    console.log("\nRequest = " + req.params.id)
+    console.log("\nGetUserById Request = " + req.params.id)
     const result = await db.getUserById(req.params.id)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.post('/user', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\nCreateUser Request = " + JSON.stringify(req.body))
     const result = await db.createUser(req.body)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
@@ -25,111 +25,112 @@ server.post('/user', async (req, res) => {
 //});
 
 server.put('/user', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\nupdateUser Request = " + JSON.stringify(req.body))
     const result = await db.updateUser(req.body)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/address/:id', async (req, res) => {
-    console.log("\nRequest = " + req.params.id)
+    console.log("\ngetAddressByUserId Request = " + req.params.id)
     const result = await db.getAddressByUserId(req.params.id)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.post('/address', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\nsaveAddress Request = " + JSON.stringify(req.body))
     const result = await db.saveAddress(req.body)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.put('/address', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\nupdateAddress Request = " + JSON.stringify(req.body))
     const result = await db.updateAddress(req.body)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/userTerms/:id', async (req, res) => {
-    console.log("\nRequest = " + req.params.id)
+    console.log("\ngetUserTermById Request = " + req.params.id)
     const result = await db.getUserTermById(req.params.id)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/terms', async (req, res) => {
+    console.log("\ngetTerms Request")
     const result = await db.getTerms()
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/terms/:id', async (req, res) => {
-    console.log("\nRequest = " + req.params.id)
+    console.log("\ngetUserTermById Request = " + req.params.id)
     const result = await db.getUserTermById(req.params.id)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.put('/terms', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\nupdateUserTerms Request = " + JSON.stringify(req.body))
     const result = await db.updateUserTerms(req.body.userId)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/plate/:id', async (req, res) => {
-    console.log("\nRequest = " + req.params.id)
+    console.log("\ngetPlateById Request = " + req.params.id)
     const result = await db.getPlateById(req.params.id)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/plate/details/:id', async (req, res) => {
-    console.log("\nRequest = " + req.params.id)
+    console.log("\ngetDetailsPlate Request = " + req.params.id)
     const result = await db.getDetailsPlate(req.params.id)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.post('/plate', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\nsavePlate Request = " + JSON.stringify(req.body))
     const result = await db.savePlate(req.body)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/plate/:latitude/:longitude/:distance', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.params))
+    console.log("\ngetPlatesByLocation Request = " + JSON.stringify(req.params))
     const result = await db.getPlatesByLocation(req.params.latitude, req.params.longitude, req.params.distance)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.post('/reserve', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\ncreateReserve Request = " + JSON.stringify(req.body))
     const result = await db.createReserve(req.body)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/reserve/comer/:status/:id', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.params))
+    console.log("\ngetReserveByComerId Request = " + JSON.stringify(req.params))
     const result = await db.getReserveByComerId(req.params)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.get('/reserve/cooker/:status/:id', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.params))
+    console.log("\ngetReserveByCookerId Request = " + JSON.stringify(req.params))
     const result = await db.getReserveByCookerId(req.params)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
 });
 
 server.put('/reserve', async (req, res) => {
-    console.log("\nRequest = " + JSON.stringify(req.body))
+    console.log("\nupdateReserve Request = " + JSON.stringify(req.body))
     const result = await db.updateReserve(req.body)
     console.log("\nResponse = " + result[0] + ' - ' + JSON.stringify(result[1]))
     return res.status(result[0]).json(result[1])
